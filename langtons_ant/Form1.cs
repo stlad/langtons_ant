ffd1abff7365bb831e4236e9152297bf0e090378
stlad <82213506+stlad@users.noreply.github.com>
2022-01-07 20:02:54 +0500

    Текстовая визуализация на форме

--- langtons_ant/Form1.cs
+++ langtons_ant/Form1.cs
@@ -15,15 +15,28 @@ namespace langtons_ant
         public Model ActiveModel;
         protected override void OnPaint(PaintEventArgs e)
         {
-            e.Graphics.DrawString(ActiveModel.Map.ToString(), new Font("Arial", 20), Brushes.Black, new Point(20, 20));
-            //e.Graphics.DrawString((-6%5).ToString(), new Font("Arial", 20), Brushes.Black, new Point(10, 10));
+            e.Graphics.DrawString(ActiveModel.Map.ToString(), new Font("Arial", 20), Brushes.Black, new Point(100, 100));
+            //e.Graphics.DrawString((-1%4).ToString(), new Font("Arial", 20), Brushes.Black, new Point(10, 10));
+
+            //e.Graphics.DrawString((ActiveModel.Map.ants[0].Direction).ToString(), new Font("Arial", 20), Brushes.Black, new Point(100, 100));
         }
 
         public Form1()
         {
             InitializeComponent();
             ActiveModel = new Model();
-            
+            var b = new Button();
+            Controls.Add(b);
+
+            var timer = new Timer();
+            timer.Interval = 500;//1000/600; 
+            timer.Tick += (sender, args) =>
+            {
+                ActiveModel.NextWorldStep();
+                Invalidate();
+            };
+            timer.Start();
         }
+
     }
 }
