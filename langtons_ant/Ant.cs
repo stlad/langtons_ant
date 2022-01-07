a5c1204b95a9657b8c834f081744d2db6eaf91a6
stlad <82213506+stlad@users.noreply.github.com>
2022-01-07 19:29:10 +0500

    Шаг вперед и поворот

--- langtons_ant/Ant.cs
+++ langtons_ant/Ant.cs
@@ -19,5 +19,48 @@ namespace langtons_ant
             X = x;
             Y = y;
         }
+
+
+        /// <summary> поворот муравья в заданном направлении. 1: вправо на 90 ;-1: лево на 90 </summary>
+        /// <param name="d">направление поворота</param>
+        private void Rotate(int d = 0)
+        {
+            var len = Enum.GetNames(typeof(Model.Direction)).Length;
+            var pos = (int)Direction;
+            Direction = (Model.Direction)((len + pos + d % len) % len);
+        }
+
+        /// <summary> шаг вдоль направления движения </summary>
+        private void StepForward()
+        {
+            switch(Direction)
+            {
+                case Model.Direction.Up:
+                    Y -= 1;
+                    break;
+                case Model.Direction.Down:
+                    Y += 1;
+                    break;
+                case Model.Direction.Left:
+                    X -= 1;
+                    break;
+                case Model.Direction.Right:
+                    X += 1;
+                    break;
+            }    
+        }
+
+        public static void GoLeft(Ant ant)
+        {
+            ant.Rotate(-1);
+            ant.StepForward();
+        }
+
+        public static void GoRight(Ant ant)
+        {
+
+        }
+
+
     }
 }
