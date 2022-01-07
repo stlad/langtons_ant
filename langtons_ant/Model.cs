d74fa71bab203ebdcc45e2256a744cec06de07f9
stlad <82213506+stlad@users.noreply.github.com>
2022-01-07 20:03:17 +0500

    словарь правил. Просчет следующих шагов всеми муравьями

--- langtons_ant/Model.cs
+++ langtons_ant/Model.cs
@@ -16,17 +16,30 @@ namespace langtons_ant
         public enum Direction
         {
             Up,
+            Right,
             Down,
-            Left,
-            Right
+            Left
         }
 
-        
+        public Dictionary<CellStates, Action<Ant>> Rules;
         public World Map { get; set; }
         public Model()
         {
-            Map = new World(5,5);
+            Map = new World(10,10,this);
+
+            Rules = new Dictionary<CellStates,Action<Ant>>();
+            Rules[CellStates.White] = Ant.GoRight;
+            Rules[CellStates.Black] = Ant.GoLeft;
         }
 
+        public void NextWorldStep()
+        {
+            foreach(var ant in Map.ants)
+            {
+                var state = ant.CheckState();
+                Rules[state](ant);
+            }
+        }
     }
+
 }
